2d44a748ebf1eead640bd588712ebcb5aa5c1426
jefferson170713 <jefferson.almeida05@aluno.ifce.edu>
2022-05-22 14:30:49 -0300

    Commitando pela 6 vez o git

---
+++ Teste_de_pasta/If_else_teste.py
@@ -0,0 +1,13 @@
+print("""
+    *************************** [J] ***************************
+    IFCE Campos caucaia                          Turno: Noturno  
+    Curso: Eng.Produção              Nome: Jefferson de Almeida
+    ***********************************************************
+""")
+
+_var = float(input("Digite um número: "))
+if _var <= 20:
+    print("O número escolhido {} é menor ou igual a 20.".format(_var))
+else:
+    print("O Número escolhido {} é maior que 20!!!.".format(_var))
+
